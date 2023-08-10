f6a08a8e43dbf327ca1a288c2ef569b002234f00
Alik Nigmatullin <NigAlik020503@yandex.ru>
2023-08-10 15:55:12 +0300

    started creation footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Topbar from "./mocks/topbar";
 import Header from "./mocks/header";
+import Footer from "./mocks/footer";
 
 function App() {
   return (
     <div>
       <Topbar/>
       <Header/>
+      <Footer/>
     </div>
   );
 }
