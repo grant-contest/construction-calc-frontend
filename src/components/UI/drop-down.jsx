4b9c41df054cb2655d2b2fc063bf6345ed5d28e9
IvanStukalov <ivanstukalov2003@gmail.com>
2023-08-14 09:10:02 +0300

    hardcoded drop down elements

--- src/components/UI/drop-down.jsx
+++ src/components/UI/drop-down.jsx
@@ -2,22 +2,43 @@ import React, {useState} from 'react';
 import axios from "axios";
 
 const DropDown = (props) => {
-  let [dropDownTitle, setDropDownTitle] = useState(props.title)
+  let [dropDownTitle, setDropDownTitle] = useState(props.title);
+  let [active, setActive] = useState(false);
 
-  axios.get('http://localhost:8000/regions')
-    .then((response) => {
-      const data = response.data;
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  const clickDropDown = () => {
+    setActive(!active);
+  }
+
+  // axios.get('http://localhost:8000/regions')   пример запроса
+  //   .then((response) => {
+  //     const data = response.data;
+  //     console.log(data);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 
   return (
-    <div className="border border-gray-300 rounded-xl w-full px-4 py-3 mb-4 flex justify-between hover:border-light-green">
-      <p className="text-gray-500 text-sm">{dropDownTitle}</p>
-      <img src="https://svoe-selo.ru/common/external/0b58c12cc41401006bb6.svg" alt=""/>
+    <div>
+      <div  // кнопка списка (хз как ее назвать)
+        className="border border-gray-300 rounded-xl w-full px-4 py-3 mb-4 flex justify-between hover:border-light-green"
+        onClick={clickDropDown}
+      >
+        <p className="text-gray-500 text-sm">{dropDownTitle}</p>
+        <img src="https://svoe-selo.ru/common/external/0b58c12cc41401006bb6.svg" alt=""/>
+      </div>
 
+      { active &&
+        <div className="border border-gray-300 rounded-xl w-full px-4 py-3 mb-4 relative bg-white"> {/* сам список */}
+          {
+            props.options.map((region) =>
+              <div className="text-gray-500">  {/* элемент списка */}
+                {region}
+              </div>
+            )
+          }
+        </div>
+      }
     </div>
   );
 };
