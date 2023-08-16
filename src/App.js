cc6d1521ac25b7eb9a0e2ead82c9b28ad9bb0239
IvanStukalov <ivanstukalov2003@gmail.com>
2023-08-16 17:05:08 +0300

    installed router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import Topbar from "./mocks/topbar";
 import Header from "./mocks/header";
 import Footer from "./mocks/footer";
 import Page0 from "./components/pages/page-0/page-0";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Topbar/>
       <Header/>
-      <Page0/>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Page0/>}/>
+        </Routes>
+      </BrowserRouter>
+
       <Footer/>
     </div>
-  );
+  )
+    ;
 }
 
 export default App;
--- src/components/pages/page-0/page-0.jsx
+++ src/components/pages/page-0/page-0.jsx
@@ -6,8 +6,7 @@ const Page0 = () => {
     <div className="flex justify-center">
       <div className="w-9/12">
         <p className="text-2xl font-medium">Калькулятор строительства</p>
-        <div className="flex justify-between">
-        {/*<div className="flex items-start">*/}
+        <div className="flex justify-between items-start">
           <img src="https://s32640.cdn.ngenix.net/images/build-house/footage/1/build-house-step-0.svg" alt=""/>
           <HomeParameters/>
         </div>
