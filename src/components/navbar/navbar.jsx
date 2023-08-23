bc0ff5a2d49a53b7f394cf94e3fd33365c05b9a2
IvanStukalov <ivanstukalov2003@gmail.com>
2023-08-23 15:08:40 +0300

    styles for navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,8 +3,20 @@ import NavbarButton from "./navbarButton";
 
 const Navbar = () => {
   return (
-    <div>
-      <NavbarButton page={1}/>
+    <div className="flex justify-center">
+      <div className="flex w-9/12 justify-between">
+        <NavbarButton page={"H"}/>
+        <NavbarButton page={1}/>
+        <NavbarButton page={2}/>
+        <NavbarButton page={3}/>
+        <NavbarButton page={4}/>
+        <NavbarButton page={5}/>
+        <NavbarButton page={6}/>
+        <NavbarButton page={7}/>
+        <NavbarButton page={8}/>
+        <NavbarButton page={9}/>
+        <NavbarButton page={"R"}/>
+      </div>
     </div>
   );
 };
--- src/components/navbar/navbarButton.jsx
+++ src/components/navbar/navbarButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavbarButton = ({page}) => {
   return (
-    <div>
-      <p>{page}</p>
+    <div className="cursor-pointer bg-light-green rounded-full w-7 h-7 flex justify-center items-center">
+      <p className="text-white">{page}</p>
     </div>
   );
 };
