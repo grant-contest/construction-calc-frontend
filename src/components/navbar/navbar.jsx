69b8154946bd6a65cd1c606ff65d86594eaaec3f
IvanStukalov <ivanstukalov2003@gmail.com>
2023-08-26 23:27:56 +0300

    finished navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import NavbarButton from "./navbarButton";
+import NavbarRow from "./navbarRow";
 
 const Navbar = () => {
   return (
     <div className="flex justify-center">
-      <div className="flex w-9/12 justify-between">
-        <NavbarButton page={"H"}/>
-        <NavbarButton page={1}/>
-        <NavbarButton page={2}/>
-        <NavbarButton page={3}/>
-        <NavbarButton page={4}/>
-        <NavbarButton page={5}/>
-        <NavbarButton page={6}/>
-        <NavbarButton page={7}/>
-        <NavbarButton page={8}/>
-        <NavbarButton page={9}/>
-        <NavbarButton page={"R"}/>
+      <div className="flex w-9/12 justify-between items-center">
+        <NavbarButton page={"https://s32640.cdn.ngenix.net/images/build-house/steps/active/first-icon.svg"} url={""} completed={true}/>
+        <NavbarRow/>
+        <NavbarButton page={1} url={"page-1"}/>
+        <NavbarRow/>
+        <NavbarButton page={2} url={"page-2"}/>
+        <NavbarRow/>
+        <NavbarButton page={3} url={"page-3"}/>
+        <NavbarRow/>
+        <NavbarButton page={4} url={"page-4"}/>
+        <NavbarRow/>
+        <NavbarButton page={5} url={"page-5"}/>
+        <NavbarRow/>
+        <NavbarButton page={6} url={"page-6"}/>
+        <NavbarRow/>
+        <NavbarButton page={7} url={"page-7"}/>
+        <NavbarRow/>
+        <NavbarButton page={8} url={"page-8"}/>
+        <NavbarRow/>
+        <NavbarButton page={9} url={"page-9"}/>
+        <NavbarRow/>
+        <NavbarButton page={"https://s32640.cdn.ngenix.net/images/build-house/steps/last-icon.svg"} url={"result"}/>
       </div>
     </div>
   );
--- src/components/navbar/navbarButton.jsx
+++ src/components/navbar/navbarButton.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
-const NavbarButton = ({page}) => {
+const NavbarButton = ({page, url, completed}) => {
   return (
-    <div className="cursor-pointer bg-light-green rounded-full w-7 h-7 flex justify-center items-center">
-      <p className="text-white">{page}</p>
-    </div>
+    <Link to={url}>
+      {completed ?
+        <div className="border border-light-green rounded-full">
+          <div className="border border-white rounded-full">
+            <div
+              className="cursor-pointer bg-light-green rounded-full w-8 h-8 flex justify-center items-center text-xs">
+              {typeof page === "number" ?
+                <p className="text-white">{page}</p>
+                :
+                <img src={page} alt="" className="w-5/12"/>
+              }
+            </div>
+          </div>
+        </div>
+        :
+        <div className="border border-gray-300 rounded-full">
+          <div className="border border-white rounded-full">
+            <div
+              className="cursor-pointer bg-gray-300 rounded-full w-8 h-8 flex justify-center items-center text-xs">
+              {typeof page === "number" ?
+                <p className="text-gray-400">{page}</p>
+                :
+                <img src={page} alt="" className="w-5/12"/>
+              }
+            </div>
+          </div>
+        </div>
+      }
+    </Link>
   );
 };
 
