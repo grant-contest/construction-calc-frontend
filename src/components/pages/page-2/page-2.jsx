347791357023452f3f5915606c1ca5530f051ce2
IvanStukalov <ivanstukalov2003@gmail.com>
2023-08-30 15:23:16 +0300

    intermediate cost

--- src/components/pages/page-2/page-2.jsx
+++ src/components/pages/page-2/page-2.jsx
@@ -6,11 +6,14 @@ import RadioGroup from "../../UI/radioGroup";
 const Page2 = () => {
   const [jobs, setJobs] = useState([]);
   const [bases, setBases] = useState([]);
+  const [cost, setCost] = useState(0);
+  let intermediateCost = 0;
 
   useEffect(() => {
     const storageJobs = localStorage.getItem("sitePreparation");
     if (storageJobs) {
       setJobs(JSON.parse(storageJobs));
+      intermediateCost += incrementIntermediateCost(JSON.parse(storageJobs));
     } else {
       axios.get('http://localhost:8000/api/site-preparation-works')
         .then((response) => {
@@ -21,12 +24,15 @@ const Page2 = () => {
     const storageBases = localStorage.getItem("baseTypes");
     if (storageBases) {
       setBases(JSON.parse(storageBases));
+      intermediateCost += incrementIntermediateCost(JSON.parse(storageBases));
     } else {
       axios.get('http://localhost:8000/api/base-types')
         .then((response) => {
           setBases(response.data);
         })
     }
+
+    setCost(intermediateCost);
   }, []);
 
   const save = () => {
@@ -34,11 +40,42 @@ const Page2 = () => {
     localStorage.setItem("baseTypes", JSON.stringify(bases))
   }
 
+  const calculateCost = (price) => {
+    setCost(cost + price);
+  }
+
+  const incrementIntermediateCost = (list) => {
+    let intermediateCost = 0;
+    for (let item of list) {
+      if (item.checked) {
+        for (let work of item.works) {
+          intermediateCost += work.price;
+        }
+      }
+    }
+    return intermediateCost;
+  }
+
+  const decrementIntermediateCost = (list) => {
+    let intermediateCost = 0;
+    for (let item of list) {
+      if (item.checked) {
+        for (let work of item.works) {
+          intermediateCost -= work.price;
+        }
+      }
+    }
+    return intermediateCost;
+  }
+
+
   return (
     <div className="flex justify-center">
       <div className="w-11/12">
-        <CheckboxGroup list={jobs}/>
-        <RadioGroup list={bases}/>
+        <p>Промежуточная стоимость {cost} ₽</p>
+
+        <CheckboxGroup list={jobs} onChange={calculateCost}/>
+        <RadioGroup list={bases} onChange={calculateCost}/>
 
         <button className="rounded-full bg-light-green text-white px-4 py-2 mt-2 mb-4"
                 onClick={save}
